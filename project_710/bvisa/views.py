7808f212630ee949674bb06f548d21b252dcc641
Sakthi Vinoth <rsakthivinoth@gmail.com>
2019-09-15 20:08:43 -0500

    typo errors fixed

--- project_710/bvisa/views.py
+++ project_710/bvisa/views.py
@@ -10,15 +10,15 @@ from django.contrib import messages
 def send_mail(employee_name, enterprise_id, project, no, start_date, end_date,cap):
 	print("sending mail")
 	server = smtplib.SMTP_SSL('smtp.gmail.com',465)
-	server.login('rsakthivinoth@gmail.com','******')
-	data = "Subject: Notification- New P710 Resource. Please assign a buddy !!\n Hey Admins, Please find the below Resource \n\nEmployee name = "+str(employee_name)+"\nEnterprise Id = "+str(enterprise_id)+"\nProject = "+str(project)+"\nContact number = "+str(no)+"\nTravel Start date = "+str(start_date)+"\nTravel end date = "+str(end_date)+"\nCapability = "+str(cap)+"\n\n\t **** This is a System generated mail. Please do not reply **** \n"
+	server.login('rsakthivinoth@gmail.com','*****')
+	data = "Subject: Notification- New P710 Resource!\n Hey Admins, Please find the below Resource and assign him a buddy \n\nEmployee name = "+str(employee_name)+"\nEnterprise Id = "+str(enterprise_id)+"\nProject = "+str(project)+"\nContact number = "+str(no)+"\nTravel Start date = "+str(start_date)+"\nTravel end date = "+str(end_date)+"\nCapability = "+str(cap)+"\n\n\t **** This is a System generated mail. Please do not reply **** \n"
 	server.sendmail("rsakthivinoth@gmail.com","rsakthivinoth@gmail.com", data)
 	server.quit()
 
 def send_succ_mail():
 	print("sending mail")
 	server = smtplib.SMTP_SSL('smtp.gmail.com',465)
-	server.login('rsakthivinoth@gmail.com','*******')
+	server.login('rsakthivinoth@gmail.com','******')
 	data = "Subject: Notification - P710 Buddy portal \n Hi..\n Thanks for registering\nFor any queries. Please reach out to mail@accenture.com \n\n\n\t **** This is a System generated mail. Please do not reply **** \n"
 	server.sendmail("rsakthivinoth@gmail.com","rsakthivinoth@gmail.com", data)
 	server.quit()
--- project_710/gcp/views.py
+++ project_710/gcp/views.py
@@ -11,15 +11,15 @@ def send_mail(employee_name, enterprise_id, project, no, start_date,cap):
 	print("sending mail")
 	server = smtplib.SMTP_SSL('smtp.gmail.com',465)
 	server.login('rsakthivinoth@gmail.com','*******')
-	data = "Subject: Notification- New P750 Resource. Please assign a buddy !!\n Hey Admins, Please find the below Resource \n\nEmployee name = "+str(employee_name)+"\nEnterprise Id = "+str(enterprise_id)+"\nProject = "+str(project)+"\nContact number = "+str(no)+"\nTravel Start date = "+str(start_date)+"\nCapability = "+str(cap)+"\n\n\t **** This is a System generated mail. Please do not reply **** \n"
+	data = "Subject: Notification- New P750 Resource.\n Hey Admins, Please find the below Resource and assign him a buddy\n\nEmployee name = "+str(employee_name)+"\nEnterprise Id = "+str(enterprise_id)+"\nProject = "+str(project)+"\nContact number = "+str(no)+"\nTravel Start date = "+str(start_date)+"\nCapability = "+str(cap)+"\n\n\t **** This is a System generated mail. Please do not reply **** \n"
 	server.sendmail("rsakthivinoth@gmail.com","rsakthivinoth@gmail.com", data)
 	server.quit()
 
 def send_succ_mail():
 	print("sending mail")
 	server = smtplib.SMTP_SSL('smtp.gmail.com',465)
-	server.login('rsakthivinoth@gmail.com','********')
-	data = "Subject: Notification - P750 Buddy portal \n Hi..\n Thanks for registering\nFor any queries. Please rach out to mail@accenture.com \n\n\n\t **** This is a System generated mail. Please do not reply **** \n"
+	server.login('rsakthivinoth@gmail.com','*******')
+	data = "Subject: Notification - P750 Buddy portal \n Hi..\n Thanks for registering\nFor any queries. Please reach out to mail@accenture.com \n\n\n\t **** This is a System generated mail. Please do not reply **** \n"
 	server.sendmail("rsakthivinoth@gmail.com","rsakthivinoth@gmail.com", data)
 	server.quit()
 
